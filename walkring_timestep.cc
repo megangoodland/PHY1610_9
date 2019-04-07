2204d1aa6b56f7f7864bc93e70fe9a473fa96cc8
megangoodland <megangoodland@gmail.com>
2019-04-07 10:26:36 -0400

    still trying to fix error

--- walkring_timestep.cc
+++ walkring_timestep.cc
@@ -39,7 +39,7 @@ void walkring_timestep(rarray<int,1>& walkerpositions, int N, double prob) {
     int Z = walkerpositions.size();
     int x = 0;
     // Start parallel, give all the threads their seeds. 
-    #pragma omp parallel for default(none) shared(Z, walkerpositions, prob, N, std::cout, std::endl) private(x) 
+    #pragma omp parallel for default(none) shared(Z, walkerpositions, prob, N, std::cout) private(x) 
     for (int i = 0; i < Z; i++) {
         if (x < 1) {
             std::cout << "this is thread: " << omp_get_thread_num() << std::endl;
