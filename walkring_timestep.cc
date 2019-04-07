cf70e2efbbe27200564ec675beea75747438f8da
megangoodland <megangoodland@gmail.com>
2019-04-07 01:45:48 -0400

    trying to fix errors with parallel

--- walkring_timestep.cc
+++ walkring_timestep.cc
@@ -42,7 +42,7 @@ void walkring_timestep(rarray<int,1>& walkerpositions, int N, double prob)
     int Z = walkerpositions.size();
 
     // Start parallel, give all the threads their seeds. Each thread will run this first code separately.
-    #pragma omp parallel default(none) shared(Z, walkerpositions, prob, N, std::cout,std::endl) private(unsigned seed){
+    #pragma omp parallel default(none) shared(Z, walkerpositions, prob, N, std::cout) private(seed){
     std::cout << "this is thread: " << omp_get_thread_num() << std::endl;
     
     seed = std::chrono::system_clock::now().time_since_epoch().count(); //getting seed using time
