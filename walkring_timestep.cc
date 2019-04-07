8004c1d17f8abba75da582a61f6104eb2af23075
megangoodland <megangoodland@gmail.com>
2019-04-07 15:02:09 -0400

    Adding comments

--- walkring_timestep.cc
+++ walkring_timestep.cc
@@ -36,29 +36,13 @@
 void walkring_timestep(rarray<int,1>& walkerpositions, int N, double prob) {
     int nthreads = omp_get_num_threads();
     int Z = walkerpositions.size();
-    int max_rand = RAND_MAX; // getting RAND_MAX to pass to parallel
-    double max_randp1 = max_rand + 1.0;
-    //rarray<int,1> seed_check(nthreads); // array saying whether or not they've gotten a seed
-    //seed_check.fill(0);
-    unsigned int seed;
-    // Start parallel, give all the threads their seeds. 
-    #pragma omp parallel default(none) shared(Z, walkerpositions, prob, N, std::cout, max_randp1) private(seed) 
+    #pragma omp parallel default(none) shared(Z, walkerpositions, prob, N, std::cout) 
     {
-       // std::cout << "Hi from thread " << omp_get_thread_num() << std::endl;
-        //std::random_device rd; // use random_device once to seed the random number generator named mt.
-        std::mt19937 mt(std::chrono::system_clock::now().time_since_epoch().count()); 
+        std::mt19937 mt(std::chrono::system_clock::now().time_since_epoch().count()); // using clock to seed
         std::uniform_real_distribution<double> dist(0.0000, 1.0000); //[0,1)
     #pragma omp for
     for (int i = 0; i < Z; i++) {
-        //seed = std::chrono::system_clock::now().time_since_epoch().count();
-            //std::mt19937 engine(std::chrono::system_clock::now().time_since_epoch().count()); //getting seed using time
-        //std::uniform_real_distribution<> uniform;
-            //seed_check[omp_get_thread_num()] = 1; // each thread goes through this if statement once
-       // double r = uniform(rand_r(&seed)); // draws a random number
-
-        //double n = rand_r(&seed); // get random number
-        //double r = n/(max_randp1); // divide random number by the max random number+1
-        double r = dist(mt);
+        double r = dist(mt); // drawing a random number
         if (r < prob) {
             // move to the right, respecting periodic boundaries
             walkerpositions[i]++;
