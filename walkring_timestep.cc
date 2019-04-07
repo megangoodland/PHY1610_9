7244edd2bac90e82ac6a3f71a14e2b8e24bca49b
megangoodland <megangoodland@gmail.com>
2019-04-07 01:33:27 -0400

    checking parallel part

--- walkring_timestep.cc
+++ walkring_timestep.cc
@@ -6,6 +6,7 @@
 
 #include "walkring_timestep.h"
 #include <omp.h>
+#include <iostream>
 #include <random>
 #include <chrono>
 
@@ -37,7 +38,8 @@
 //
 void walkring_timestep(rarray<int,1>& walkerpositions, int N, double prob)
 {   
-    //int nthreads = omp_get_num_threads();
+    int nthreads = omp_get_num_threads();
+    std::cout << 'this is thread: ' << omp_get_thread_num() << ' out of ' << nthreads << std::endl;
     int Z = walkerpositions.size();
 
     // Start parallel, give all the threads their seeds. Each thread will run this first code separately.
